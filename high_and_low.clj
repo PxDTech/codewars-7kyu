38925dd41ae2f7def2211e3402cc0cad0caf3856
PxDTech <118567236+PxDTech@users.noreply.github.com>
2025-09-01 06:38:04 -0400

    First commit
    ;; In this little assignment you are given a string of space separated numbers,
    ;; and have to return the highest and lowest number.

---
+++ high_and_low.clj
@@ -0,0 +1,9 @@
+(ns pasquale.high-and-low
+  (:gen-class))
+
+;; In this little assignment you are given a string of space separated numbers,
+;; and have to return the highest and lowest number.
+
+(defn high-and-low
+  [s]
+  (read-string s))
